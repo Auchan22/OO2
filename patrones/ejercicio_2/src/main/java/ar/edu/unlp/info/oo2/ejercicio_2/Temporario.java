dc0e18a1c7fd97d371c08ae4f7519e2bba781bc1
Auchan22 <agustinsurila@gmail.com>
2025-04-06 17:23:25 -0300

    ejercicio 2

---
+++ patrones/ejercicio_2/src/main/java/ar/edu/unlp/info/oo2/ejercicio_2/Temporario.java
@@ -0,0 +1,33 @@
+package ar.edu.unlp.info.oo2.ejercicio_2;
+
+public class Temporario implements Empleado {
+	
+	private int horasTrabajadas;
+	private boolean esCasado;
+	private int cantHijos;
+
+	@Override
+	public double calculaBasico() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double calcularDescuento() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double calcularAdicional() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double sueldo() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
